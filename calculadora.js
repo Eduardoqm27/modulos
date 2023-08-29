4f6d7758c01357f56c999c5b20a851f925be0ad4
IFC-CAS <IFC-CAS>
2023-08-29 12:04:37 -0300

    bazinga

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,11 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var divisao = require("./modulos/divisao");
+var subtrair = require("./modulos/subtrair");
+var multiplicacao = require("./modulos/multiplicacao");
 
 console.log(somar(20,90));
 console.log(media(20,90));
+console.log(divisao(20,90));
+console.log(subtrair(20,90));
+console.log(multiplicacao(20,90));
